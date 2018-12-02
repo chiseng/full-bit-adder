b2f01916baf535d238a773b11b2bb123932eff48
chiseng <thehedge96@hotmail.com>
2018-12-03 00:53:47 +0800

    edited the gentop rng, fsm for mojo top

--- LEDdisplay/LEDdisplay.mojo
+++ LEDdisplay/LEDdisplay.mojo
@@ -1,23 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <project name="LEDdisplay" board="Mojo V3" language="Lucid">
   <files>
-    <src>generator_top.luc</src>
-    <src>led_matrix.luc</src>
-    <src>check.luc</src>
-    <src top="true">mojo_top.luc</src>
-    <src>leddisplay.luc</src>
-    <src>timer.luc</src>
-    <src>generator_bottom.luc</src>
-    <src>adder.luc</src>
-    <src>multi_seven_seg.luc</src>
     <src>multiply.luc</src>
     <src>alu.luc</src>
     <src>boolean.luc</src>
-    <src>compare16.luc</src>
+    <src>timer.luc</src>
+    <src>check.luc</src>
+    <src>multi_seven_seg.luc</src>
+    <src top="true">mojo_top.luc</src>
+    <src>leddisplay.luc</src>
+    <src>generator_top.luc</src>
     <src>shifter.luc</src>
-    <ucf lib="true">mojo.ucf</ucf>
+    <src>generator_bottom.luc</src>
+    <src>seven_seg.luc</src>
+    <src>compare16.luc</src>
+    <src>adder.luc</src>
+    <src>led_matrix.luc</src>
     <ucf>user.ucf</ucf>
-    <component>counter.luc</component>
+    <ucf lib="true">mojo.ucf</ucf>
+    <component>pn_gen.luc</component>
+    <component>decoder.luc</component>
     <component>reset_conditioner.luc</component>
+    <component>pipeline.luc</component>
+    <component>button_conditioner.luc</component>
+    <component>counter.luc</component>
   </files>
 </project>
---
+++ LEDdisplay/work/project.tcl
@@ -0,0 +1,22 @@
+set projDir "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/planAhead"
+set projName "LEDdisplay"
+set topName top
+set device xc6slx9-2tqg144
+if {[file exists "$projDir/$projName"]} { file delete -force "$projDir/$projName" }
+create_project $projName "$projDir/$projName" -part $device
+set_property design_mode RTL [get_filesets sources_1]
+set verilogSources [list "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/mojo_top_0.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/reset_conditioner_1.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/generator_bottom_2.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/generator_top_3.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/check_4.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/counter_5.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/led_matrix_6.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/leddisplay_7.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/alu_8.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/alu_8.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/alu_8.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/counter_11.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/counter_12.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/adder_13.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/boolean_14.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/compare16_15.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/shifter_16.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/multiply_17.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/adder_13.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/boolean_14.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/compare16_15.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/shifter_16.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/multiply_17.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/adder_13.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/boolean_14.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/compare16_15.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/shifter_16.v" "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/work/verilog/multiply_17.v"]
+import_files -fileset [get_filesets sources_1] -force -norecurse $verilogSources
+set ucfSources [list "C:/Users/Wong/Documents/GitHub/full-bit-adder/LEDdisplay/constraint/user.ucf" "C:/Program\ Files/Mojo\ IDE/library/components/mojo.ucf"]
+import_files -fileset [get_filesets constrs_1] -force -norecurse $ucfSources
+set_property -name {steps.bitgen.args.More Options} -value {-g Binary:Yes -g Compress} -objects [get_runs impl_1]
+set_property steps.map.args.mt on [get_runs impl_1]
+set_property steps.map.args.pr b [get_runs impl_1]
+set_property steps.par.args.mt on [get_runs impl_1]
+update_compile_order -fileset sources_1
+launch_runs -runs synth_1
+wait_on_run synth_1
+launch_runs -runs impl_1
+wait_on_run impl_1
+launch_runs impl_1 -to_step Bitgen
+wait_on_run impl_1
